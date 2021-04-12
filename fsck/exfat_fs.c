b0b761b0b3dfcb6fcbaf70675121734a812a154f
Hyunchul Lee <hyc.lee@gmail.com>
2021-04-12 15:29:45 +0900

    fsck: change cluster argument of bitmap operations
    Change the cluster argument of bitmap operations
    to start with EXFAT_FIRST_CLUSTER instead of 0.
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- fsck/exfat_fs.c
+++ fsck/exfat_fs.c
@@ -34,13 +34,12 @@ void exfat_bitmap_set_range(struct exfat *exfat, char *bitmap,
 	clus_t clus;
 
 	if (!heap_clus(exfat, start_clus) ||
-		!heap_clus(exfat, start_clus + count))
+		!heap_clus(exfat, start_clus + count - 1))
 		return;
 
 	clus = start_clus;
 	while (clus < start_clus + count) {
-		EXFAT_BITMAP_SET(bitmap,
-				clus - EXFAT_FIRST_CLUSTER);
+		exfat_bitmap_set(bitmap, clus);
 		clus++;
 	}
 }
