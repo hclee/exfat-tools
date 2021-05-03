a6daae81922d45e317a365ea657d9190dad8b27f
Hyunchul Lee <hyc.lee@gmail.com>
2021-05-03 15:25:51 +0900

    fsck: handle bad clusters properly in check_clus_chain()
    Truncate a NotFat file if a next cluster is BAD, And
    even if a next cluster is out of range, allocate the
    cluster to a file
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- fsck/fsck.c
+++ fsck/fsck.c
@@ -165,18 +165,29 @@ static int check_clus_chain(struct exfat_de_iter *de_iter,
 		/* This cluster is allocated or not */
 		if (get_inode_next_clus(exfat, node, clus, &next))
 			goto truncate_file;
-		if (!node->is_contiguous) {
-			if (!heap_clus(exfat, next) &&
-					next != EXFAT_EOF_CLUSTER) {
+		if (next == EXFAT_BAD_CLUSTER) {
 				if (repair_file_ask(de_iter, node,
 						ER_FILE_INVALID_CLUS,
-						"broken cluster chain. "
-						"truncate to %"
+						"BAD cluster. truncate to %"
 						PRIu64 " bytes",
 						count * exfat->clus_size))
 					goto truncate_file;
-
 				else
+					return -EINVAL;
+		} else if (!node->is_contiguous) {
+			if (next != EXFAT_EOF_CLUSTER &&
+			    !heap_clus(exfat, next)) {
+				if (repair_file_ask(de_iter, node,
+						ER_FILE_INVALID_CLUS,
+						"broken cluster chain. truncate to %"
+						PRIu64 " bytes",
+						(count+1) * exfat->clus_size)) {
+					count++;
+					prev = clus;
+					exfat_bitmap_set(exfat->alloc_bitmap,
+							 clus);
+					goto truncate_file;
+				} else
 					return -EINVAL;
 			}
 		}
