fcb101f1c649f23d763071953e8c55bff35a619e
Hyunchul Lee <hyc.lee@gmail.com>
2021-05-03 14:56:53 +0900

    fsck: add file offset to exfat_lookup_filter
    Make exfat_lookup_dentry_set store the file
    offset of a found directory entry to
    the exfat_lookup_filter strcuture.
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- fsck/de_iter.h
+++ fsck/de_iter.h
@@ -32,5 +32,6 @@ int exfat_de_iter_get_dirty(struct exfat_de_iter *iter,
 int exfat_de_iter_flush(struct exfat_de_iter *iter);
 int exfat_de_iter_advance(struct exfat_de_iter *iter, int skip_dentries);
 off_t exfat_de_iter_device_offset(struct exfat_de_iter *iter);
+off_t exfat_de_iter_file_offset(struct exfat_de_iter *iter);
 
 #endif
