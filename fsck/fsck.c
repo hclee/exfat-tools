8cad13260c2cb7f9ecdc99eab5084d3e56dfb752
Hyunchul Lee <hyc.lee@gmail.com>
2021-05-26 16:43:39 +0900

    fsck: repair unknown directory entries
    if unknown directory entries are found,
    Clear InUse bit in EntryType field.
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- fsck/fsck.c
+++ fsck/fsck.c
@@ -89,11 +89,15 @@ static void usage(char *name)
 			##__VA_ARGS__);			\
 })
 
-#define repair_file_ask(iter, inode, code, fmt, ...)	\
-({							\
-		resolve_path_parent(&path_resolve_ctx,	\
-				(iter)->parent, inode);	\
-		exfat_repair_ask(&exfat_fsck, code,	\
+#define repair_file_ask(iter, inode, code, fmt, ...)			\
+({									\
+		if (inode)						\
+			resolve_path_parent(&path_resolve_ctx,		\
+					    (iter)->parent, inode);	\
+		else							\
+			resolve_path(&path_resolve_ctx,			\
+				     (iter)->parent);			\
+		exfat_repair_ask(&exfat_fsck, code,			\
 			"ERROR: %s: " fmt " at %#" PRIx64,		\
 			path_resolve_ctx.local_path,			\
 			##__VA_ARGS__,					\
@@ -954,24 +958,27 @@ static int read_children(struct exfat_fsck *fsck, struct exfat_inode *dir)
 			} else
 				free_exfat_inode(node);
 			break;
-		case EXFAT_VOLUME:
-			if (!read_volume_label(de_iter)) {
-				exfat_err("failed to verify volume label\n");
-				ret = -EINVAL;
-				goto err;
-			}
-			break;
-		case EXFAT_BITMAP:
-		case EXFAT_UPCASE:
-			break;
 		case EXFAT_LAST:
 			goto out;
+		case EXFAT_VOLUME:
+		case EXFAT_BITMAP:
+		case EXFAT_UPCASE:
+			if (dir == exfat->root)
+				break;
+			/* fall through */
 		default:
 			if (IS_EXFAT_DELETED(dentry->type))
 				break;
-			exfat_err("unknown entry type. 0x%x\n", dentry->type);
-			ret = -EINVAL;
-			goto err;
+			if (repair_file_ask(de_iter, NULL, ER_DE_UNKNOWN,
+					    "unknown entry type %#x at %07" PRIx64,
+					    dentry->type,
+					    exfat_de_iter_file_offset(de_iter))) {
+				struct exfat_dentry *dentry;
+
+				exfat_de_iter_get_dirty(de_iter, 0, &dentry);
+				dentry->type &= EXFAT_DELETE;
+			}
+			break;
 		}
 
 		exfat_de_iter_advance(de_iter, dentry_count);
