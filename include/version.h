7581c55c569e94ce830fe593809d657cb5669fe1
Hyunchul Lee <hyc.lee@gmail.com>
2022-08-22 15:28:55 +0900

    exfatfsck: release webos-1.1.2-10
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- include/version.h
+++ include/version.h
@@ -6,6 +6,6 @@
 #ifndef _VERSION_H
 
 #define EXFAT_PROGS_VERSION "1.1.2"
-#define WEBOS_EXFAT_PROGS_VERSION "webos-1.1.2-9"
+#define WEBOS_EXFAT_PROGS_VERSION "webos-1.1.2-10"
 
 #endif /* !_VERSION_H */
