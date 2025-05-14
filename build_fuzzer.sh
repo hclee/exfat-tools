e3dc9a881664bf5a1086b97b17e1f8b2096ed27b
Hyunchul Lee <hyc.lee@gmail.com>
2025-05-15 08:55:35 +0900

    fuzzer: add undefined sanitizer
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- build_fuzzer.sh
+++ build_fuzzer.sh
@@ -9,7 +9,7 @@ SRC="fuzz_test/test01.cc"
 #clang++ -g -Wdeprecated -fpermissive -fsanitize=address,fuzzer $SRC -o test01 -I ./include -I ./fuzz_test -I ./fsck -L./lib -lexfat
 
 make clean
-CC=clang LDFLAGS="-fsanitize=address,fuzzer-no-link" ./configure
+CC=clang CFLAGS=-g LDFLAGS="-fsanitize=address,undefined,fuzzer-no-link" ./configure
 make -j7
 
 echo "archiving objects..."
@@ -17,6 +17,6 @@ objcopy --strip-symbol main fsck/fsck.o
 ar rcs libfsck.a fsck/fsck.o fsck/repair.o lib/exfat_dir.o lib/exfat_fs.o lib/libexfat.o
 
 echo "building fuzzer..."
-clang++ -Wall -fsanitize=fuzzer,address -Wno-cpp -Wpedantic -std=c++11 -g $SRC -o test01 -I ./include -I ./fuzz_test -I ./fsck libfsck.a
+clang++ -Wall -g -fsanitize=fuzzer,address,undefined -Wno-cpp -Wpedantic -std=c++11 -g $SRC -o test01 -I ./include -I ./fuzz_test -I ./fsck libfsck.a
 
 cp fuzz_test/test01 ~/qemu-linux/host-share-dir/fuzz_test/
