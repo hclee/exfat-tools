b5f23c079630c568471cd32fe1b9eaf4e5d7ae62
Hyunchul Lee <hyc.lee@gmail.com>
2025-05-15 09:52:07 +0900

    fuzzer: add exfat fsck fuzzing
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- fuzz_test/test01.cc
+++ fuzz_test/test01.cc
@@ -16,31 +16,10 @@ extern "C" {
 #include "fsck.h"
 }
 
-int test_read_boot_sect(const char *filename)
-{
-	struct exfat_user_input ui;
-	struct exfat_blk_dev bd;
-	struct pbr *bs;
-	int ret;
-
-	memset(&ui, 0, sizeof(ui));
-	memset(&bd, 0, sizeof(bd));
-
-	ui.dev_name = filename;
-	ret = exfat_get_blk_dev_info(&ui, &bd);
-	if (ret < 0) {
-		printf("ERROR: exfat_get_blk_dev_info\n");
-		return ret;
-	}
-
-	ret = exfat_boot_region_check(&bd, &bs, false);
-	close(bd.dev_fd);
-	return ret;
-}
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 	const char *filename = "/tmp/exfat_test_file";
 	int fd;
+	struct fsck_user_input ui;
 
 	fd = open(filename, O_RDWR|O_CREAT|O_TRUNC);
 	if (fd < 0) {
@@ -55,6 +34,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 	}
 	close(fd);
 
-	test_read_boot_sect(filename);
-	return 0;
+	memset(&ui, 0, sizeof(ui));
+	ui.ei.dev_name = filename;
+	ui.ei.writeable = false;
+	ui.options = FSCK_OPTS_REPAIR_NO;
+
+	return exfat_fsck_main(&ui);
 }
